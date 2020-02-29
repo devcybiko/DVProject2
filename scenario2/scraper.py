778254df6ab9f79de4c18be9006dde6789e7f877
Greg Smith <thinkable@thinkable.us>
2020-02-29 17:50:00 -0500

    automatic commit

--- scenario2/scraper.py
+++ scenario2/scraper.py
@@ -13,7 +13,7 @@ def scrape_info():
     browser = init_browser()
 
     # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+    url = "https://www.nrablog.com/rss"
     browser.visit(url)
 
     #time.sleep(1)
@@ -23,27 +23,14 @@ def scrape_info():
     soup = bs(html, "html.parser")
 
     # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    links = soup.findAll('link')
+    print(links[0].text)
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
-
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
-
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[3]["src"]
-    sloth_img = url + relative_image_path
-
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
 
     # Close the browser after scraping
     browser.quit()
 
     # Return results
-    return costa_data
+    return links
+
+scrape_info()
