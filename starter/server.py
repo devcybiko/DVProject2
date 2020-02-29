2f987b4990cfd517c073bb72b96bd827b49ea87d
Greg Smith <thinkable@thinkable.us>
2020-02-29 15:50:00 -0500

    automatic commit

--- starter/server.py
+++ starter/server.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, render_template
 ### create a flask instance
 app = Flask(__name__)
 
-sampleData = [
+someWeeklyPerformanceData = [
     {"day": "Sunday", "value": 15339},
     {"day": "Monday", "value": 21345},
     {"day": "Tuesday", "value": 18483},
@@ -12,11 +12,25 @@ sampleData = [
     {"day": "Friday", "value": 24092},
     {"day": "Saturday", "value": 12034}
 ]
-    
+
+theDataForSalaries = [
+        {'Salaries':1200000, 'Office':20000, 'Merchandise':80000, 'Legal':2000, 'Total':12120000},
+        {'Salaries':1300000, 'Office':20000, 'Merchandise':70000, 'Legal':2000, 'Total'},
+        {'Salaries', 'Office', 'Merchandise', 'Legal', 'Total'},
+        {'Salaries', 'Office', 'Merchandise', 'Legal', 'Total'},
+        [, , , ],
+        [, , , , ],
+        [1300000, 20000, 120000, 2000, 131222000],
+        [1400000, 20000, 90000, 2000, 14102000]];
+
 ### one of your APIs
 @app.route("/api/weeklydata")
-def weeklyData():
-    return jsonify(sampleData)
+def the_Weekly_Data_Method():
+    return jsonify(someWeeklyPerformanceData)
+
+@app.route("/api/salarydata")
+def the_Method_for_some_Salary_Data():
+    return jsonify(theDataForSalaries)
 
 @app.route("/")
 def home():
--- starter/static/js/app.js
+++ starter/static/js/app.js
@@ -1,24 +1,51 @@
 // this function is run when the page loads
 function main() {
     console.log("Beginning main()");
-    console.log(d3.json);
+    weeklyChart();
+    salaryTable();
+}
+
+function weeklyChart() {
     d3.json("/api/weeklydata").then(rows => {
         console.log(rows);
         let data = sqlToPlotly(rows, "day", "value");
-        let options = {margin: {t: 0}};
+        let options = { margin: { t: 0 } };
         let chartDiv = document.getElementById('chart');
-        Plotly.newPlot(chartDiv, data, options);
+        Plotly.newPlot(chartDiv, [data], options); // remember - plotly wants an array of data (one per trace)
     });
+}
+
+function salaryTable() {
+
+    var data = [{
+        type: 'table',
+        header: {
+            values: [["<b>EXPENSES</b>"], ["<b>Q1</b>"],
+            ["<b>Q2</b>"], ["<b>Q3</b>"], ["<b>Q4</b>"]],
+            align: "center",
+            line: { width: 1, color: 'black' },
+            fill: { color: "grey" },
+            font: { family: "Arial", size: 12, color: "white" }
+        },
+        cells: {
+            values: values,
+            align: "center",
+            line: { color: "black", width: 1 },
+            font: { family: "Arial", size: 11, color: ["black"] }
+        }
+    }]
+
+    Plotly.newPlot('table', data);
 }
 
 function sqlToPlotly(rows, xname, yname) {
     let x = [];
     let y = [];
-    for(row of data) {
+    for (row of rows) {
         x.push(row[xname]);
         y.push(row[yname]);
     }
-    return {x, y};
+    return { x, y };
 }
 
 
