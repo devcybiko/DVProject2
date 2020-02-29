f200bf6e62cced59f185a1fe03f6904a12ed1423
Greg Smith <thinkable@thinkable.us>
2020-02-29 18:40:00 -0500

    automatic commit

--- scenario2/scraper.py
+++ scenario2/scraper.py
@@ -16,10 +16,14 @@ def scrape_rss_feed(url):
     links = []
     var_url = urlopen(url)
     xmldoc = parse(var_url)
+    parent_map = {c:p for p in xmldoc.iter() for c in p}
     for item in xmldoc.iterfind('.//link'):
         print(item.text)
-        byline = item.find('...')
+        div = parent_map
+        print(div)
+        byline = div.find('.//span')
         print(byline)
+
         slashes = item.text.split('/')
         if (len(slashes) > 4) : links.append(item.text)
 
