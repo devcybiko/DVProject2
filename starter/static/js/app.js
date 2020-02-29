422cd04515663195b5896fa63f5e5ee91c38e201
Greg Smith <thinkable@thinkable.us>
2020-02-29 15:40:00 -0500

    automatic commit

--- starter/static/js/app.js
+++ starter/static/js/app.js
@@ -2,10 +2,24 @@
 function main() {
     console.log("Beginning main()");
     console.log(d3.json);
-    d3.json("/api/weeklydata").then(data =>console.log(data));
+    d3.json("/api/weeklydata").then(rows => {
+        console.log(rows);
+        let data = sqlToPlotly(rows, "day", "value");
+        let options = {margin: {t: 0}};
+        let chartDiv = document.getElementById('chart');
+        Plotly.newPlot(chartDiv, data, options);
+    });
 }
 
-
+function sqlToPlotly(rows, xname, yname) {
+    let x = [];
+    let y = [];
+    for(row of data) {
+        x.push(row[xname]);
+        y.push(row[yname]);
+    }
+    return {x, y};
+}
 
 
 
