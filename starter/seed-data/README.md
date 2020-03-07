40f0dc4e06cbb59491c5b753f86428e89703bea9
Greg Smith <thinkable@thinkable.us>
2020-03-07 16:07:03 -0500

    automatic push

---
+++ starter/seed-data/README.md
@@ -0,0 +1,16 @@
+# SEED DATA
+
+Your seed data will be different. This is just an example of how to create a table and load it with initial data.
+
+## Week 09, Day 01, Activity 09
+
+This is the same data as we used in Week 9, Day 1 "SQL". See the 09-Ins_Joins activity.
+
+## Running this...
+
+Open the `PG Admin 4` app
+
+Create a database
+* In the left sidebar, 
+* Open "PostgreSQL 12"
+  * Open "Databases"
---
+++ starter/seed-data/schema.sql
@@ -0,0 +1,32 @@
+-- Create the seed data from week 09.01 Activity 9
+DROP TABLE IF EXISTS matches
+
+CREATE TABLE matches
+loser_age NUMBER,
+loser_id INT,
+loser_name VARCHAR(64),
+loser_rank INT,
+winner_age NUMBER,
+winner_id INT,
+winner_name VARCHAR(64),
+winner_rank INT
+);
+
+DROP TABLE IF EXISTS players;
+CREATE TABLE players (
+	player_id INT,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	hand VARCHAR,
+	country_code VARCHAR
+);
+
+-- 'import' the .csv files
+-- BE SURE TO UPDATE THE PATH TO YOUR FULLY QUALIFIED PATH
+-- BE SURE TO UPDATE THE COLUMN NAMES TO MATCH YOUR TABLE
+COPY matches(loser_age,loser_id,loser_name,loser_rank,winner_age,winner_id,winner_name,winner_rank) 
+FROM '/Users/greg/git/DVProject2/starter/seed-data/matches.csv' DELIMITER ',' CSV HEADER;
+
+COPY players(player_id,first_name,last_name,hand,country_code) 
+FROM '/Users/greg/git/DVProject2/starter/seed-data/players.csv' DELIMITER ',' CSV HEADER;
+
